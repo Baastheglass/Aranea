1bdb92eb8385821cb52d7108795b59cf5d1abad4
Baastheglass <lightningblazer123@gmail.com>
2026-02-07 20:33:40 +0500

    updating backend cors config to allow vercel in

--- backend/app.py
+++ backend/app.py
@@ -103,7 +103,10 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://aranea.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
